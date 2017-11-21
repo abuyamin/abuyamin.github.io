97254eacc803b90a840d413fd5f7fc30a3de25c2
hash <hashim_mekelle@yahoo.com>
2017-11-21 02:15:01 +0000

    updated shuffle indexing, working repeat all button and collpsible play list tray

--- aplayer/js/script.js
+++ aplayer/js/script.js
@@ -1,37 +1,50 @@
 $(document).ready(function(){
-  var AP = new Object();
+  window.AP = new Object();
   // Check for the various File API support.
   if (window.File && window.FileReader && window.FileList && window.Blob) {
     // Great success! All the File APIs are supported.
   } else {
-    alert('The File APIs are not fully supported in this browser.');
+    alert("The File APIs are not fully supported in this browser.");
   }
+
+  // convert millisecond duration into minute and second
+  window.secsToHours = function(seconds){
+    var minutes = Math.floor(seconds / 60);
+    seconds = (seconds % 60).toFixed(0);
+    if(minutes>60){
+      var hr = Math.floor(minutes / 60);
+      var m = (minutes % 60).toFixed(0);
+      minutes = hr + ":" + (m < 10 ? "0" : "") + m;
+    }
+    return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+  };
 
   // shuffle array
   window.shuffle = function (array) {
     let counter = array.length;
 
     // While there are elements in the array
+    var index, temp;
     while (counter > 0) {
       // Pick a random index
-      let index = Math.floor(Math.random() * counter);
+      index = Math.floor(Math.random() * counter);
       // Decrease counter by 1
       counter--;
       // And swap the last element with it
-      let temp = array[counter];
+      temp = array[counter];
       array[counter] = array[index];
       array[index] = temp;
     }
     return array;
-  }
+  };
 
-  // Arreay generation 
+  // Arreay generation
   window.Array.range = (start, end) => Array.from({length: (end - start)}, (v, k) => k + start);
   window.sortNumber = function(a,b) {
     return a - b;
   };
 
-  // load input file and start processing it 
+  // load input file and start processing it
   function HandleFileSelect(){
     var file = this.files[0];
 
@@ -49,7 +62,7 @@ $(document).ready(function(){
 
     var trackList = xmlDoc.querySelectorAll("trackList > track");
 
-    // load track information if it exist 
+    // load track information if it exist
     function getTag(element, tag) {
       var res = element.getElementsByTagName(tag);
       if(res.length==0){
@@ -58,26 +71,15 @@ $(document).ready(function(){
         return res[0].childNodes[0].nodeValue;
       }
     }
-    // convert millisecond duration into minute and second 
-    window.millToMinutes = function(millis) {
-      var minutes = Math.floor(millis / 60000);
-      var seconds = ((millis % 60000) / 1000).toFixed(0);
-      if(minutes >= 60){
-        var hr = Math.floor(minutes / 60);
-        var newMinutes = (minutes % 60).toFixed(0);
-        minutes = hr + ":" + newMinutes;
-      }
-      return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
-    }
 
     // iterate over trackList generate tracks list of object
     var tracks = [];
     for(var i=0, t; t=trackList[i]; i++){
-      var duration = getTag(t, 'duration');
+      var duration = getTag(t, "duration");
       var track = {track: i,
-       name: getTag(t, 'title'), 
-       file: getTag(t, 'location'), 
-       length: millToMinutes(duration)};
+       name: getTag(t, "title"), 
+       file: getTag(t, "location"), 
+       length: secsToHours(duration/1000)};
       tracks.push(track); // append trucks object to the array
     }
     if(tracks.length){
@@ -88,130 +90,147 @@ $(document).ready(function(){
 
   function setupPlayer(tracks){
     // clear playlist for loading a new one 
-    var elm = document.getElementById('plList');
+    var elm = document.getElementById("plList");
     while (elm.hasChildNodes()) {
       elm.removeChild(elm.lastChild);
     }
 
-    var playActive = false,
-    buildPlaylist = $.each(tracks, function(key, value) {
+    
+    $.each(tracks, function(key, value) {
       var trackNumber = value.track,
       trackName = value.name,
       trackLength = value.length;
       if (trackNumber.toString().length === 1) {
-        trackNumber = '&#160&#160;' + trackNumber;
+        trackNumber = "&#160&#160;" + trackNumber;
       } else if (trackNumber.toString().length === 2){
-       trackNumber = '&#160;' + trackNumber;
+       trackNumber = "&#160;" + trackNumber;
      }else {
-      trackNumber = '' + trackNumber;
+      trackNumber = "" + trackNumber;
     }
     $('#plList').append('<tr class="plItem"><td class="plNum">' + trackNumber + '.</td><td class="plTitle">' + trackName + '</td><td class="plLength">' + trackLength + '</td></tr>');
       // $('#plList').append('<li><div class="plItem"><div class="plNum">' + trackNumber + '.</div><div class="plTitle">' + trackName + '</div><div class="plLength">' + trackLength + '</div></div></li>');
-      // $('#plList').append('<li><div class="plItem"><span class="plNum">' + trackNumber + '.</span><span class="plTitle">' + trackName + '</span><span class="plLength">' + trackLength + '</span></div></li>');
     });
-    trackCount = tracks.length,
-    npAction = $('#npAction'),
-    npTitle = $('#npTitle');
-    AP.shuffleIndex = Array.range(0, trackCount);
+    AP.playActive = false;
+    AP.shuffleOn = false;
+    AP.repeatOn = false;
     AP.index = 0;
+    AP.trackCount = tracks.length;
+    AP.shuffleIndex = Array.range(0, AP.trackCount);
+    var npAction = $("#npAction");
+    var npTitle = $("#npTitle");
+
+    AP.shuffleOn = function(){
+      return $('#btnShuffle').hasClass('control-button--active')  
+    }
+    AP.repeatOn = function(){
+      return $('#btnRepeat').hasClass('control-button--active') 
+    }
+    AP.playlistOn = function(){
+      return $('#btnPlaylist').hasClass('control-button--active') 
+    }
     // check if shuffle is on
-    if ( $('#btnShuffle').hasClass('control-button--active') ){
+    if (AP.shuffleOn()){
       shuffle(AP.shuffleIndex);
+      AP.shuffleOn = true;
+    }
+    if (AP.playlistOn()) {
+      $("#plwrap").show();
+    } else {
+      $("#plwrap").hide();
     }
 
     var audio = $('#audio1').bind('play', function () {
-      playActive = true;
+      AP.playActive = true;
       npAction.text('Now Playing...');
     }).bind('pause', function () {
-      playActive = false;
+      AP.playActive = false;
       npAction.text('Paused...');
     }).bind('ended', function () {
       npAction.text('Paused...');
-      if ((AP.index + 1) < trackCount) {
-        AP.index++;
-        loadTrack(AP.index);
-        audio.play();
-      } else {
-        audio.pause();
-        AP.index = 0;
-        loadTrack(AP.index);
-      }
-    }).get(0),
-    btnPrev = $('#btnPrev').click(function () {
-      if ((AP.index - 1) > -1) {
-        AP.index--;
-        loadTrack(AP.index);
-        if (playActive) {
-          audio.play();
-        }
-      } else {
-        audio.pause();
-        AP.index = 0;
-        loadTrack(AP.index);
-      }
-    }),
-    btnNext = $('#btnNext').click(function () {
-      if ((AP.index + 1) < trackCount) {
-        AP.index++;
-        loadTrack(AP.index);
-        if (playActive) {
-          audio.play();
-        }
-      } else {
-        audio.pause();
-        AP.index = 0;
-        loadTrack(AP.index);
-      }
-    }),
-    li = $('#plList tr').click(function () {
-      var id = parseInt($(this).index());
-      if (id !== AP.index) {
-        playTrack(id, true);
-      }
-    }),
+      playNextAudio(1);
+    }).get(0);
 
-    loadTrack = function (id, direct) {
-      if(direct===undefined){ direct = false;}
-      // translte id into effective id using AP.shuffleIndex
-      if(!direct){
-        id = AP.shuffleIndex[id % AP.shuffleIndex.length];
+    $('#btnPrev').click(function () {
+      playNextAudio(-1, AP.playActive);
+    });
+    
+    $('#btnNext').click(function () {
+      playNextAudio(1, AP.playActive);
+    });
+
+    $('#plList tr').click(function () {
+      var id = parseInt($(this).index());
+      var id_index = AP.shuffleIndex.indexOf(id)
+      if (id_index !== AP.index) {
+        AP.index = id_index;
+        playTrack(true);
       }
+    });
+
+    var loadTrack = function (id) {
+      id = AP.shuffleIndex[id];
       $('.plSel').removeClass('plSel');
       $('#plList tr:eq(' + id + ')').addClass('plSel');
       // scroll to current line 
       var topPos = document.getElementById('plList').rows[id].offsetTop;
       document.getElementById('plwrap').scrollTop = topPos - 46*5;;
-
-      npTitle.text(tracks[id].track +' >> '+ tracks[id].name);
-      AP.index = id;
-      audio.src = tracks[id].file;
       
-    },
-
-    playTrack = function (id, direct) {
-      loadTrack(id, direct);
-      audio.play();
-      audio.onloadedmetadata = function() {
-        $('#plList tr:eq(' + AP.index + ') td:eq(2)').html(millToMinutes(audio.duration*1000));
-        console.log('current id : '+ id + ' index : '+ AP.index);
-      };
-      // var a = $('#audio1')[0];
-      // console.log("Track duration \t "+ millToMinutes(a.duration*1000));
+      npTitle.text(tracks[id].name + " | Track " + id.toString());
+      audio.src = tracks[id].file;
     };
-    playTrack(AP.index);
+
+    var playTrack = function(playStatus){
+      playStatus = (typeof playStatus !== 'undefined') ?  playStatus : true;
+      loadTrack(AP.index);
+      if (playStatus) {
+        audio.play();
+        audio.onloadedmetadata = function() {
+          var c_id = AP.shuffleIndex[AP.index];
+          $('#plList tr:eq(' + c_id + ') td:eq(2)').html(secsToHours(audio.duration));
+          // console.log('current id : '+ c_id + ' index : '+ AP.index);
+        };
+      }
+
+    };
+
+    var playNextAudio = function(dxn, playStatus){
+      playStatus = (typeof playStatus !== 'undefined') ?  playStatus : true;
+      var nextIndex = AP.index + dxn;
+      if(nextIndex > -1 && nextIndex < AP.trackCount){
+        AP.index = nextIndex;
+        playTrack(playStatus);
+      } else {
+        if(!AP.repeatOn()){
+          audio.pause();
+          playStatus = false;
+        }
+        playTrack(playStatus);
+      }
+    };
+
+    playTrack(true);
   }
   // listen to any update on the shuffling 
   $('#btnShuffle').click(function() {
     // change shuffle status 
     $('#btnShuffle').toggleClass('control-button--active');
-    if ( $('#btnShuffle').hasClass('control-button--active') ){
-      shuffle(AP.shuffleIndex);
-    } else {
-      AP.shuffleIndex.sort(sortNumber);
+    if(typeof AP.shuffleIndex != 'undefined'){
+      if (AP.repeatOn() ){
+        AP.index = 0;
+        shuffle(AP.shuffleIndex);
+      } else {
+        AP.index = AP.shuffleIndex[AP.index]
+        AP.shuffleIndex.sort(sortNumber);
+      }
     }
   });  
   $('#btnRepeat').click(function(){
     $('#btnRepeat').toggleClass('control-button--active');
+  });
+
+  $('#btnPlaylist').click(function(){
+    $('#btnPlaylist').toggleClass('control-button--active');
+    $("#plwrap").toggle("slow");
   });
 
 
